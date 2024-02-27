7ca18833bdc17adab9b8e8ffc5331404f01b68ff
David Forstenlechner <dfo@vizrt.com>
2024-02-27 10:05:45 +0100

    Added the actual SHA512 for test project 1

--- ports/test-project-a/portfile.cmake
+++ ports/test-project-a/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO dforsten/test_project_a
     REF 1b4924080d7367f33a3297959f1bc62d2cdefc71
-    SHA512 0
+    SHA512 c6142f4cc099c053e461bb550e1483ce38ce374f29ec3b93210edd6feec730a776c5162e0b686942958185e0650dd796e0447d5ba8ec761b95eb98c519dd9028
 )
 
 vcpkg_cmake_configure(
